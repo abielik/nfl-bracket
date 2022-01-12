7c1612d012195cd382a77a8a773eb1d3d8091e03
Alan Bielik <abielik92@gmail.com>
2022-01-11 21:09:20 -0500

    data

---
+++ public/data.js
@@ -0,0 +1,62 @@
+const data = {
+  AFC: {
+    '1 Seed': {
+      city: 'Tennessee',
+      name: 'Titans',
+    },
+    '2 Seed': {
+      city: 'Kansas City',
+      name: 'Chiefs',
+    },
+    '3 Seed': {
+      city: 'Buffalo',
+      name: 'Bills',
+    },
+    '4 Seed': {
+      city: 'Cincinnati',
+      name: 'Bengals',
+    },
+    '5 Seed': {
+      city: 'Las Vegas',
+      name: 'Raiders',
+    },
+    '6 Seed': {
+      city: 'New England',
+      name: 'Patriots',
+    },
+    '7 Seed': {
+      city: 'Pittsburgh',
+      name: 'Steelers',
+    },
+  },
+  NFC: {
+    '1 Seed': {
+      city: 'Green Bay',
+      name: 'Packers',
+    },
+    '2 Seed': {
+      city: 'Tampa Bay',
+      name: 'Buccaneers',
+    },
+    '3 Seed': {
+      city: 'Dallas',
+      name: 'Cowboys',
+    },
+    '4 Seed': {
+      city: 'Los Angeles',
+      name: 'Rams',
+    },
+    '5 Seed': {
+      city: 'Arizona',
+      name: 'Cardinals',
+    },
+    '6 Seed': {
+      city: 'San Francisco',
+      name: '49ers',
+    },
+    '7 Seed': {
+      city: 'Philadelphia',
+      name: 'Eagles',
+    },
+  },
+};
---
+++ src/components/Cards.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Cards() {
+  return (
+    <div className="card">
+      <div class="card-title">Wildcard</div>
+      <div class="team-top">team-top</div>
+      <div class="team-bottom">team-bottom</div>
+    </div>
+  );
+}
